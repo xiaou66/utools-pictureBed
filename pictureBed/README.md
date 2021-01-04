629074c6e627c3d1b4aa864e73e1ba06b166fc5c
小u <2630316030@qq.com>
2021-01-04 20:02:28 +0800

    修复 牛图网 图床

--- pictureBed/README.md
+++ pictureBed/README.md
@@ -20,9 +20,10 @@
 
 > 特别感谢 takashiki  对图床插件的更新
 
-1. 丰富腾讯云cos功能
+1. 丰富 腾讯云cos 功能
     - 增加可选选项 `自定义域名`(自有域名，需[自行配置](https://cloud.tencent.com/document/product/436/11142))
     - 增加可选选项 `图片样式`（需开启[数据万象](https://cloud.tencent.com/document/product/460/36540))
+2. 修复 牛图网 图床
 
 ### v1.1.0
 
--- picturebed-vue/src/api/niupic.js
+++ picturebed-vue/src/api/niupic.js
@@ -2,13 +2,13 @@ function niupic () {}
 niupic.uploadImage = async (item, id) => {
   const formData = new FormData()
   formData.append('image_field', item)
-  return await fetch('https://www.niupic.com/index/upload/process', {
+  return await fetch('https://www.niupic.com/api/upload', {
     method: 'POST',
     body: formData
   }).then(res => res.json())
     .then(res => {
       if (res.code === 200) {
-        return { status: 200, url: `https://${res.data}`, id }
+        return { status: 200, url: `${res.data}`, id }
       } else {
         return { status: 403, message: '上传失败' }
       }
